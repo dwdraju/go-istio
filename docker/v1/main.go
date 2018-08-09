13a2f2e8cc025bfc7d1d0fe94cb6753330000e2b
alpha <rajudawadinp@gmail.com>
2018-08-09 12:29:22 +0545

    add docker for 2 version

---
+++ docker/v1/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+    "encoding/json"
+    "github.com/gorilla/mux"
+    "log"
+    "net/http"
+    "fmt"
+)
+
+type Version struct {
+    Version int
+}
+
+func home(w http.ResponseWriter, r *http.Request){
+    version := Version{1}
+    js, err := json.Marshal(version)
+    if err != nil {
+        http.Error(w, err.Error(), 500)
+        return
+    }
+    w.Header().Set("Content-Type","application/json")
+    w.Write(js)
+}
+
+func main() {
+    router := mux.NewRouter()
+    port   := 8080
+    addr := fmt.Sprintf(":%d", port)
+    router.HandleFunc("/", home).Methods("GET")
+    fmt.Printf("App is running on http://localhost:%d !\n", port)
+    log.Fatal(http.ListenAndServe(addr, router))
+}
